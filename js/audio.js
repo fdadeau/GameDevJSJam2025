cbf3cb917731911eab3f80d43df84d21bef60d35
fdadeau <frederic.dadeau@univ-fcomte.fr>
2025-04-26 15:52:31 +0200

    update audio paths

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,9 @@
 
 export const audio = {
-    music: makeAudio("../sounds/PuzzlePeace.mp3", 0.2, true),
-    move: makeAudio("../sounds/swing2.mp3",0.6),
-    victory: makeAudio("../sounds/victory.mp3"),
-    defeat: makeAudio("../sounds/defeat.mp3")
+    music: makeAudio("./sounds/PuzzlePeace.mp3", 0.2, true),
+    move: makeAudio("./sounds/swing2.mp3",0.6),
+    victory: makeAudio("./sounds/victory.mp3"),
+    defeat: makeAudio("./sounds/defeat.mp3")
 }
 
 function makeAudio(file, volume, loop) {
